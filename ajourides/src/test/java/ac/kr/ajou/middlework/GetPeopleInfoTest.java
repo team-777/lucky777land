85142520e26546ae67c749f223d03e6faf4cde44
JaeHyeung <jaehuneng@gmail.com>
2019-07-21 23:13:43 +0900

    create runningtime test

--- ajourides/src/test/java/ac/kr/ajou/middlework/GetPeopleInfoTest.java
+++ ajourides/src/test/java/ac/kr/ajou/middlework/GetPeopleInfoTest.java
@@ -4,6 +4,7 @@ import ac.kr.ajou.domain.PersonInfo;
 import org.junit.Before;
 import org.junit.Test;
 import ac.kr.ajou.middlework.GetPeopleInfo;
+import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +13,12 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GetPeopleInfoTest {
+    @Mock
+    private GetPeopleInfo getPeopleInfo;
+    private PeopleIntoGroups peopleIntoGroups;
+
     @Test
-    public void setparamTest(){ //201520969 이재형
+    public void 각_놀이기구에_따라_minage가_설정되는지_테스트(){ //201520969 이재형
         GetPeopleInfo getPeopleInfo = new GetPeopleInfo();
         PeopleIntoGroups peopleIntoGroups = new PeopleIntoGroups();
         List<PersonInfo> peopleList = new ArrayList<>();
@@ -30,7 +35,14 @@ public class GetPeopleInfoTest {
         getPeopleInfo.PeopleInfo(peopleList.subList(0,60), 60);
         assertThat(getPeopleInfo.getMinage(),is(10));
     }
-}
 
-@Test
-public void
+    @Test(timeout = 5000)
+    public void MerryGoRound가4초안에_끝나는지_테스트(){ //201520969
+        GetPeopleInfo getPeopleInfo = new GetPeopleInfo();
+        PeopleIntoGroups peopleIntoGroups = new PeopleIntoGroups();
+        List<PersonInfo> peopleList = new ArrayList<>();
+        peopleList = peopleIntoGroups.GroupsIntoNull(peopleList,20);
+
+        getPeopleInfo.PeopleInfo(peopleList, 20);
+    }
+}
---
+++ ajourides/src/test/java/ac/kr/ajou/middlework/PeopleIntoGroupsTest.java
@@ -0,0 +1,7 @@
+package ac.kr.ajou.middlework;
+
+import org.junit.Test;
+
+public class PeopleIntoGroupsTest {
+   
+}
